43b2db9290bb3e521fd9f84d3677e9785efb8fec
Maynard G. Krebs <chrisharges@gmail.com>
2026-02-28 19:49:57 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const APP_CONFIG = {
+  appName: "Rome-O-Matic",
+  backendBase: "https://rome-o-matic-backend.onrender.com"
+};
+
+const API_URL = `${APP_CONFIG.backendBase}/api/articles`;
+const SONNET_URL = `${APP_CONFIG.backendBase}/api/sonnet`;
+const TOPICS_URL = `${APP_CONFIG.backendBase}/api/topics`;
